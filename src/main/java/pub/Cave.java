cefeac4feba69edf31f0d272b3a5dcee93032111
aymed10 <ayoubmedzouhri10@gmail.com>
2025-02-26 00:35:44 +0100

    Vector to ArrayList Cave.java

--- src/main/java/pub/Cave.java
+++ src/main/java/pub/Cave.java
@@ -1,51 +1,65 @@
 package pub;
 
-import java.util.Vector; 
-import java.util.Enumeration;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author Pierre Le Fameux
  *
  */
 public class Cave {
-	public Vector<Boisson> rayons;
-	
+	public List<Boisson> rayons; // Changed from Vector to List
+
 	/**
-	 * 
+	 * Constructor
 	 */
-	public Cave(){
-		this.rayons = new Vector<Boisson>();
+	public Cave() {
+		this.rayons = new ArrayList<>(); // Changed from Vector to ArrayList
 	}
-	
+
 	/**
-	 * @param b
+	 * Add a Boisson to the Cave
+	 *
+	 * @param b Boisson to add
 	 */
-	public void add(Boisson b){
+	public void add(Boisson b) {
 		this.rayons.add(b);
 	}
-	
+
 	/**
-	 * @param nom
-	 * @return
+	 * Take a Boisson from the Cave by name
+	 *
+	 * @param nom Name of the Boisson
+	 * @return The Boisson if found, otherwise null
 	 */
-	public Boisson take(String nom){
+	public Boisson take(String nom) {
 		Boisson retour = null;
-		Enumeration e = this.rayons.elements ();
-		while (e.hasMoreElements () && !((retour=(Boisson)e.nextElement()).nom.equalsIgnoreCase(nom))){}
-		this.rayons.remove(retour);
-		return retour;
+		Iterator<Boisson> iterator = this.rayons.iterator(); // Using Iterator instead of Enumeration
+		while (iterator.hasNext()) {
+			retour = iterator.next();
+			if (retour.nom.equalsIgnoreCase(nom)) {
+				iterator.remove();
+				return retour;
+			}
+		}
+		return null;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
-	public String toString(){
-		String retour = new String();
-		String eol = System.getProperty("line.separator"); 
-		Enumeration e = this.rayons.elements ();
-		retour = retour + "Cave : " + eol;
-		while (e.hasMoreElements ()) {
-			retour = retour + "\t" + e.nextElement().toString() + eol;
+	@Override
+	public String toString() {
+		StringBuilder retour = new StringBuilder();
+		String eol = System.getProperty("line.separator");
+
+		retour.append("Cave : ").append(eol);
+
+		for (Boisson boisson : this.rayons) {
+			retour.append("\t").append(boisson.toString()).append(eol);
 		}
-		return retour;
+
+		return retour.toString();
 	}
 }
