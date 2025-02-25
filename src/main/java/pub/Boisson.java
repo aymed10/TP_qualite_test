1beb2523156c25f7c5527e6033e76a98b37be9ad
aymed10 <ayoubmedzouhri10@gmail.com>
2025-02-26 00:29:18 +0100

    Removed Boolean constructor Boisson.java

--- src/main/java/pub/Boisson.java
+++ src/main/java/pub/Boisson.java
@@ -1,43 +1,47 @@
 package pub;
 
-
 /**
  * @author Pierre Le Fameux
  *
  */
 public class Boisson {
 	public String nom;
-	public Boolean alcoolise;
+	public boolean alcoolise;
 	public Float degre;
-	public Float volume; // volume de la bouteille, cennette, fut ...
+	public Float volume;
 	public Float prix;
-	
+
 	/**
+	 * Constructor for non-alcoholic drink
+	 *
 	 * @param nom
 	 */
-	public Boisson(String nom){
+	public Boisson(String nom) {
 		this.nom = nom;
-		this.alcoolise = new Boolean(false);
+		this.alcoolise = false;
 	}
-	
+
 	/**
+	 * Constructor for alcoholic drink
+	 *
 	 * @param nom
 	 * @param degre
 	 */
-	public Boisson(String nom, Float degre){
+	public Boisson(String nom, Float degre) {
 		this.nom = nom;
 		this.degre = degre;
-		this.alcoolise = new Boolean(true);
+		this.alcoolise = true;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
-	public String toString(){
+	@Override
+	public String toString() {
 		String retour = this.nom;
-		if(this.alcoolise){
+		if (this.alcoolise) {
 			return retour + " (l'abus d'alcool est dangereux pour la sante)";
-		}else{
+		} else {
 			return retour;
 		}
 	}
